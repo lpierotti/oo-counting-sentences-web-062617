d9ed9324fc674ea9555440e8706dced4fe97354e
Luke Pierotti <lpierotti2@gmail.com>
2017-06-28 17:19:18 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+  	self.end_with?("?")
   end
 
   def exclamation?
-
+  	self.end_with?("!")
   end
 
   def count_sentences
-
+  	splitSentence = self.split(/[.!?]/)
+  	splitSentence.delete_if do |word|
+  		word == ""
+  	end
+  	splitSentence.length
   end
 end
